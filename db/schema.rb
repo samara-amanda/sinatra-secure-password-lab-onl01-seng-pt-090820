7424ea88bc24e356f1b9bddf1afbc737d7d8eb09
Samara Grant <samara.amanda01@gmail.com>
2020-12-02 02:42:43 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20201202022604) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password_digest"
+    t.string "password"
   end
 
 end
